a65619e95631c3af8e601d37ede388de9c745a52
jurec72 <jurec72@gmail.com>
2018-07-02 10:57:33 -0500

    new updates

--- src/CodingJava.java
+++ src/CodingJava.java
@@ -3,6 +3,9 @@ public class CodingJava {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		for(int i=0;i<3;i++) {
+			System.out.println("Say Hello to me");
+		}
 	}
 
 }
